b318bf6bd9f2556f67ba71f44256f73a0d60bc1e
Amajuoyi chidera <Amajuoyichideradavid@gmail.com>
2023-06-10 15:12:54 -0700

    first version

--- main.py
+++ main.py
@@ -55,14 +55,84 @@
 
 # weatherChecker(weather)
 
+# num1 = int(input('Enter the first number : '))
+# num2 = int(input('Enter the second number : '))
 
-num1 = int(input('Enter the first number : '))
-num2 = int(input('Enter the second number : '))
+# def bigger(num1, num2) :
+#   if num1 > num2 :
+#     print(f'The Bigger Number is {num1}')
+#   else :
+#     print(f'The Bigger Number is {num2}')
 
-def bigger(num1, num2) :
-  if num1 > num2 :
-    print(f'The Bigger Number is {num1}')
-  else :
-    print(f'The Bigger Number is {num2}')
+# bigger(num1, num2)
 
-bigger(num1, num2)
+
+# fruits = ['banana' , 'apple', 'oramge']
+# # print(fruits)
+# fruits.append('strawberry')
+# # print(fruits[2])
+# print(fruits[0:3])
+
+
+# Dictionary
+# person = { 'name' : 'david',
+#           'ambition' : 'software engineer',
+#           'age' : '19',
+#           'dream country' : 'germany' }
+
+# print(person['name'])
+# print(person['age'])
+# print(person['ambition'])
+# print(person['dream country'])
+
+
+# def introducer():
+#   person = {
+#     'name' : 'david',
+#     'ambition' : 'software engineer',
+#     'age' : 19,
+#     'dream_country' : 'germany' 
+#   }
+
+#   person['age'] = 20
+#   print(f"Hi my name is {person['name']} im {person['age']} years old, my ambition in life is to become a very good {person['ambition']} and my dream country is {person['dream_country']}")
+
+# introducer()
+
+# # tuple
+# numbers = (1,2)
+# print(numbers)
+
+# sets
+
+# language1 = ['Javascript', 'python' , 'Java', 'python']
+# language2 = ['Java', 'Javascript', 'sql', 'nodeJs', 'nodeJs']
+# language3 = set(language1 + language2)
+# print(language3)
+
+# items = ['sql', 'sql', 'node']
+  
+# def unique(items):
+#   return (list(set(items))) 
+
+# print(unique(items))
+
+# loops
+fruits = ['banana' , 'apple', 'oramge', 'paw paw']
+
+# for fruit in enumerate(fruits):
+#   print(f'Fruit : {fruit} ')
+
+# # tupel unpacking
+# for index, fruit in enumerate(fruits):
+#   print(f'Fruit : {fruit} {index}')
+
+#  range
+
+# for _ in range(5):
+#   fruits.append('orange')
+
+# print(fruits)
+
+
+# while loops
